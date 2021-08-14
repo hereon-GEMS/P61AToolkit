7ce52faef28c9cc369cccf88dcf7eded68007888
Gleb Dovzhenko <dovjenko.g@gmail.com>
2021-08-14 18:37:28 +0200

    lib update [3]

--- src/py61a/py61a/stress/sin2psi.py
+++ src/py61a/py61a/stress/sin2psi.py
@@ -13,7 +13,7 @@ class Sin2PsiObject:
         self.y = ydata
         self.depth = depth
 
-        ids = np.isnan(self.x) | np.isnan(self.y) |np.isnan(self.depth)
+        ids = np.isnan(self.x) | np.isnan(self.y) | np.isnan(self.depth)
         self.x, self.y, self.depth = self.x[~ids], self.y[~ids], self.depth[~ids]
 
         if self.x.size == 0 or self.y.size == 0:
