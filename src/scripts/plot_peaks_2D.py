57b2223dcc00df095f9d3803b547a5abec0c2d28
Gleb Dovzhenko <dovjenko.g@gmail.com>
2021-08-17 10:53:25 +0200

    lib update [3]

---
+++ src/scripts/plot_peaks_2D.py
@@ -0,0 +1,48 @@
+from py61a.viewer_utils import read_peaks, valid_peaks, peak_id_str
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+if __name__ == '__main__':
+    dd = read_peaks(r'Z:\p61\2021\data\11010463\raw\2a\experiments\2aYscan_02000\Peaks_2ayscan.csv')
+    x_mot = 'eu.chi'
+    y_mot = 'eu.y'
+
+    # custom selection rule
+    # dd = dd[np.isclose(dd['md']['eu.phi'], 90., rtol=1e-2)]
+
+    for peak_id in valid_peaks(dd, valid_for='phase'):
+
+        plt.figure(peak_id_str(dd, peak_id))
+
+        ax11 = plt.subplot(221, projection='3d')
+        ax11.scatter(dd['md'][x_mot], dd['md'][y_mot], dd[peak_id]['height'])
+        ax11.set_title('Height')
+        ax11.set_xlabel(x_mot)
+        ax11.set_ylabel(y_mot)
+        ax11.set_zlabel('[cts]')
+
+        ax12 = plt.subplot(222, projection='3d')
+        ax12.set_title(r'$R_{wp}^2$')
+        ax12.set_ylabel(r'$R_{wp}^2$')
+        ax12.scatter(dd['md'][x_mot], dd['md'][y_mot], dd[peak_id]['rwp2'])
+        ax12.set_xlabel(x_mot)
+        ax12.set_ylabel(y_mot)
+
+        ax21 = plt.subplot(223, projection='3d')
+        ax21.set_title('Center')
+        ax21.scatter(dd['md'][x_mot], dd['md'][y_mot], dd[peak_id]['center'])
+        ax21.set_xlabel(x_mot)
+        ax21.set_ylabel(y_mot)
+        ax21.set_zlabel('[keV]')
+
+        ax22 = plt.subplot(224, projection='3d')
+        ax22.set_title('Sigma')
+        ax22.scatter(dd['md'][x_mot], dd['md'][y_mot], dd[peak_id]['sigma'])
+        ax22.set_xlabel(x_mot)
+        ax21.set_ylabel(y_mot)
+        ax22.set_zlabel('[keV]')
+
+        plt.tight_layout()
+
+    plt.show()
