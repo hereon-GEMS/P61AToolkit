7b44ce708370bfc5ead6c8e6fe569557aa1cc8c0
Gleb Dovzhenko <dovjenko.g@gmail.com>
2021-09-10 09:19:34 +0200

    Build

--- src/apps/Viewer/P61App.py
+++ src/apps/Viewer/P61App.py
@@ -77,7 +77,7 @@ class P61App(QApplication):
 
     """
     name = 'P61A::Viewer'
-    version = '1.0.1' + ' build 2021-08-24'
+    version = '1.0.1' + ' build 2021-09-10'
 
     dataRowsInserted = pyqtSignal(int, int)
     dataRowsRemoved = pyqtSignal(list)
