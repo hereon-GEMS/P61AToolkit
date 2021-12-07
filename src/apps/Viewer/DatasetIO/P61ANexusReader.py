60d505b008807a79313a4d8c6670f1f443c33988
Gleb Dovzhenko <dovjenko.g@gmail.com>
2021-12-07 18:11:37 +0100

    Added option for summing up frames in nxs files or taking them separately

--- src/apps/Viewer/DatasetIO/P61ANexusReader.py
+++ src/apps/Viewer/DatasetIO/P61ANexusReader.py
@@ -44,49 +44,54 @@ class P61ANexusReader:
                 if '/'.join(channel + self.hist) not in f:
                     continue
 
-                frames = np.sum(f['/'.join(channel + self.hist)], axis=0)
-                frames[:20] = 0.0
-                frames[-1] = 0.0
-
-                # corrections to NIST Pb and W lines
-                kev = np.arange(frames.shape[0]) * kev_per_bin
-                if ii == 0:
-                    kev = np.arange(frames.shape[0]) * 0.050494483569344 + 0.029899315869827
-                elif ii == 1:
-                    kev = np.arange(frames.shape[0]) * 0.04995786201326 + 0.106286326963684
+                if sum_frames:
+                    frames = [np.sum(f['/'.join(channel + self.hist)], axis=0)]
                 else:
-                    kev = (np.arange(frames.shape[0]) + 0.5) * kev_per_bin
+                    frames = f['/'.join(channel + self.hist)]
 
-                if self._replace:
-                    frames[frames < 1.0] = 1.0
-                else:
-                    self.logger.warning('NeXuS import filters out intensities <1 ct. '
-                                        'Not all imported datasets have the same shape, this might bring unexpected '
-                                        'consequences!')
-                    kev, frames = kev[frames >= 1.0], frames[frames >= 1.0]
+                for frame in frames:
+                    frame[:20] = 0.0
+                    frame[-1] = 0.0
 
-                if self.q_app is not None:
-                    row = {c: None for c in self.q_app.data.columns}
-                else:
-                    row = {c: None for c in self.columns}
+                    # corrections to NIST Pb and W lines
+                    kev = np.arange(frame.shape[0]) * kev_per_bin
+                    if ii == 0:
+                        kev = np.arange(frame.shape[0]) * 0.050494483569344 + 0.029899315869827
+                    elif ii == 1:
+                        kev = np.arange(frame.shape[0]) * 0.04995786201326 + 0.106286326963684
+                    else:
+                        kev = (np.arange(frame.shape[0]) + 0.5) * kev_per_bin
 
-                if ('/'.join(channel + self.all_event) in f) and ('/'.join(channel + self.all_good) in f):
-                    allevent = np.sum(f['/'.join(channel + self.all_event)], axis=0)
-                    allgood = np.sum(f['/'.join(channel + self.all_good)], axis=0)
-                    row.update({'DeadTime': 1. - allgood / allevent})
+                    if self._replace:
+                        frame[frame < 1.0] = 1.0
+                    else:
+                        self.logger.warning('NeXuS import filters out intensities <1 ct. '
+                                            'Not all imported datasets have the same shape, this might bring unexpected '
+                                            'consequences!')
+                        kev, frame = kev[frame >= 1.0], frame[frame >= 1.0]
 
-                row.update({
-                    'DataX': kev,
-                    'DataY': frames,
-                    'DataID': f_name + ':' + '/'.join(channel),
-                    'Channel': ii,
-                    'ScreenName': os.path.basename(f_name) + ':' + '%02d' % ii,
-                    'Active': True,
-                })
+                    if self.q_app is not None:
+                        row = {c: None for c in self.q_app.data.columns}
+                    else:
+                        row = {c: None for c in self.columns}
 
-                if self.q_app is not None:
-                    row.update({'Color': next(self.q_app.params['ColorWheel'])})
-                result.loc[result.shape[0]] = row
+                    if ('/'.join(channel + self.all_event) in f) and ('/'.join(channel + self.all_good) in f):
+                        allevent = np.sum(f['/'.join(channel + self.all_event)], axis=0)
+                        allgood = np.sum(f['/'.join(channel + self.all_good)], axis=0)
+                        row.update({'DeadTime': 1. - allgood / allevent})
+
+                    row.update({
+                        'DataX': kev,
+                        'DataY': frame,
+                        'DataID': f_name + ':' + '/'.join(channel),
+                        'Channel': ii,
+                        'ScreenName': os.path.basename(f_name) + ':' + '%02d' % ii,
+                        'Active': True,
+                    })
+
+                    if self.q_app is not None:
+                        row.update({'Color': next(self.q_app.params['ColorWheel'])})
+                    result.loc[result.shape[0]] = row
 
         result = result.astype('object')
         result[pd.isna(result)] = None
